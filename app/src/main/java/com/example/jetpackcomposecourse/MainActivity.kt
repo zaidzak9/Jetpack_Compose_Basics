49bb408e9ef6c7c9af903a55f17383776bb5dcb9
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-07-25 12:28:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposecourse/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.jetpackcomposecourse
+
+import android.os.Bundle
+import android.text.Layout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
